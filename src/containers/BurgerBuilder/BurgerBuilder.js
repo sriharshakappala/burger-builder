b9b2290d78784bb0caa0dec9cba9ff9d263bd603
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-08-19 00:00:42 +0530

    Add dynamic burger ingredients based on state of Burger componenet

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,25 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 1,
+      cheese: 1,
+      meat: 1
+    }
+  }
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>
           Build Controls
         </div>
