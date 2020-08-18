8c9e47fec0e0b6962be1bbdcc8d78af0697fcd06
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-08-18 23:02:39 +0530

    Add Aux and Layout skeleton components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout>
+          <p>
+            Test
+          </p>
+        </Layout>
       </div>
     );
   }
