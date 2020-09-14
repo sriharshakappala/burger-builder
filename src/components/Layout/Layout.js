ab890faa9c72b4bb3ce8c02b484f7b4f45ef58e1
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-09-14 16:02:19 +0530

    Add logo component and layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = (props) => (
   <Aux>
     <div>
+      <Toolbar />
       Toolbar, SideDrawer, Backdrop
     </div>
     <main className={classes.Content}>
