4af74d582823201330c549e1253983084ee3ce18
Sri Harsha Kappals <sriharsha.kappala@hotmail.com>
2020-09-15 22:13:33 +0530

    Side drawer improvements

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-  <Aux>
-    <div>
-      <Toolbar />
-      <SideDrawer />
-      Toolbar, SideDrawer, Backdrop
-    </div>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  render() {
+    return (
+      <Aux>
+        <div>
+          <Toolbar />
+          <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        </div>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+
+}
+
+export default Layout;
